6fe44534273988093130782e0df18e631f0276bb
KatSym <katerina.symiakaki@igb.berlin.de>
2025-07-09 13:21:11 +0200

    m2.e plot

--- models.R
+++ models.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(brms)
 library(tidybayes)
+library(modelr)
 
 # load data
 load("./data/sites_FC_phyto.RData")
@@ -93,43 +94,46 @@ trt.cols <- c(`C`= "#000000", #black - C
               `I`= "#4472ca", #blu - I
               `E`= "#e84855") 
 
-edat.pred <- edat %>% 
-  group_by(mesocosm, ExpDay, Treatment, fun_grp) %>%
-  add_epred_draws(m2.e,
-                  re_formula = NA,
-  ) 
+fil.cols <- c(`C`= "#00000030", #black - C
+              `D`= "#0a875430", #g - D
+              `I`= "#4472ca30", #blu - I
+              `E`= "#e8485530") 
+  
 
 edat.pred <- edat %>% 
-  expand_grid(treat = c("C", "D", "I", "E"),
-              fungr = c("I", "II", "III", "IV", "V", "VI", "VII"), 
-              expday = c(0, 4, 12, 20, 28, 36),
-              vol.off = 1)
-# 
-# edat.pred %>% 
-#   ggplot(aes(x = ExpDay,
-#                 y = count,
-#                 colour = Treatment)
-#   ) +
-#   stat_lineribbon(aes(y = (.epred)),
-#                   .width = c(0.95)
-#                   # position = position_dodge(.5),
-#                   # linewidth = 2
-#   )+
-#   geom_point(mapping = aes(x = ExpDay,
-#                  y = count,
-#                  colour = Treatment),
-#              alpha = .35
-#              # position = position_jitterdodge(dodge.width = .5),
-#   ) +
-#   # facet_wrap(~ fun_grp
-#   #            , scales = "free_y"
-#   # )+
-#   scale_color_manual(values = trt.cols)
-#   # scale_fill_manual(values = trt.cols)+
-#   # labs(title = "Heterotroph", 
-#   #      y = "<span style='font-size: 15pt'>Abundance </span>
-#   #        <span style='font-size: 13pt'>Log(x+1) cells mL\u207b\u00b9</span>",
-#   #      x = NULL)+
+  data_grid(expand_grid(Treatment = c("C", "D", "I", "E"),
+              fun_grp = c("I", "II", "III", "IV", "V", "VI", "VII"), 
+              ExpDay = c(0, 4, 12, 20, 28, 36),
+              vol.offset = 1)) %>% 
+  add_epred_draws(m2.e, re_formula = NA) 
+
+(plot <- edat.pred %>%
+  ggplot(aes(x = ExpDay,
+             y = .epred,
+             colour = Treatment)
+  ) +
+  stat_lineribbon(aes(y = (.epred),
+                      fill = Treatment),
+                  .width = c(0.95)
+                  # alpha = .3
+                  # position = position_dodge(.5),
+                  # linewidth = 2
+  )+
+  geom_point(edat, mapping = aes(x = ExpDay,
+                                 y = count/vol.offset,
+                                 colour = Treatment),
+             alpha = .35,
+             position = position_jitterdodge(dodge.width = .5),
+  ) +
+  facet_wrap(~ fun_grp
+             , scales = "free_y"
+  )+
+  scale_color_manual(values = trt.cols) +
+  scale_fill_manual(values = fil.cols) )
+  labs(title = "Heterotroph",
+       y = "<span style='font-size: 15pt'>Abundance </span>
+         <span style='font-size: 13pt'>Log(x+1) cells mL\u207b\u00b9</span>",
+       x = NULL)
 #   # theme(axis.title.y = ggtext::element_markdown(),
 #   #       axis.title.x = element_text(size = 15),
 #   #       plot.title = element_text(size=16, face="italic")
