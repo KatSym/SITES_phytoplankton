8cbe6e154860097ecf136135b1dd5805886e6a71
Angelos Amyntas <angelos.amyntas@posteo.net>
2025-07-24 13:25:57 +0200

    tadannn

--- models2.R
+++ models2.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(brms)
+library(emmeans)
 library(tidybayes)
 library(modelr)
 library(see)
@@ -62,8 +63,7 @@ edat_fgroup = edat_tax %>%
             biovol = sum(taxonvol),
             vol.offset = mean(vol.offset)) %>% 
   mutate(dens = count/vol.offset,
-         voldens = biovol/vol.offset,
-         ExpDay = as.factor(ExpDay)) %>% 
+         voldens = biovol/vol.offset) %>% 
   ungroup()
 
 
@@ -80,7 +80,7 @@ mtot = brm(
   warmup = 3000,
   cores = 4,
   control = list(adapt_delta = 0.99),
-  seed = 543,
+  seed = 123,
   backend = "cmdstanr", 
   data = edat_tot
 ) 
@@ -111,15 +111,14 @@ unique(edat_tot[,1:3])  %>%
   facet_wrap(vars(Treatment))
 
 
-library(emmeans)
-
-
 mtot %>% emmeans("Treatment", by = "ExpDay",
-                  at = list(ExpDay = c(0,4,12,20,28,36))) %>% 
+                 at = list(ExpDay = c(0,4,12,20,28,36))
+                 #at = list(ExpDay = seq_range(edat_tot$ExpDay, n = 31))
+                 ) %>% 
   contrast(method = "trt.vs.ctrl") %>%
   gather_emmeans_draws() %>% 
   ggplot(aes(x = ExpDay, y = .value, fill = contrast)) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "darkgrey") +
   stat_lineribbon(aes(y = .value), .width = c(0.95), #alpha = .5,
                   point_interval = "mean_qi",
                   linewidth = .25) + 
@@ -132,3 +131,118 @@ mtot %>% emmeans("Treatment", by = "ExpDay",
   xlab("Experimental Day") +
   theme_light() +
   facet_wrap(vars(contrast))
+
+
+#### group-level biovolume #####################################################
+
+mgroup = brm(
+  bf(
+    log(voldens) ~ s(ExpDay, by = interaction(fun_grp, 
+                                              Treatment), 
+                     k = 5) +
+      (ExpDay + fun_grp + ExpDay:fun_grp | mesocosm),
+    sigma ~ fun_grp
+  ),
+  family = gaussian(),
+  prior = prior(normal(0,5), class = "b")+
+          prior(exponential(2), class = "sd"),
+  init = 0,
+  chains = 4,
+  iter = 6000,
+  warmup = 3000,
+  cores = 4,
+  control = list(adapt_delta = 0.99,
+                 max_treedepth = 12),
+  seed = 543,
+  backend = "cmdstanr", 
+  data = edat_fgroup
+) 
+saveRDS(mgroup, "mgroup6000.RDS")
+mgroup = readRDS("mgroup6000.RDS")
+pp_check(mgroup, ndraws = 100)
+summary(mgroup)
+
+unique(edat_fgroup[,1:4])  %>% 
+  add_epred_draws(mgroup, re_formula = NULL) %>%
+  ggplot(aes(x = ExpDay, y = log(voldens), fill = Treatment)) +
+  stat_lineribbon(aes(y = .epred), .width = c(0.95), #alpha = .5,
+                  point_interval = "mean_qi",
+                  linewidth = .25) + 
+  geom_point(data = edat_fgroup, 
+             color = "black", alpha = .5, size = 1,
+             position = position_jitter(.9)) +
+  scale_fill_manual(values = c("#00000030",
+                               "#0a875430",
+                               "#4472ca30",
+                               "#e8485530")) +
+  scale_x_continuous(breaks = c(0,4,12,20,28,36)) +
+  ylab(expression(log(mu*m^3/mL))) +
+  xlab("Experimental Day") +
+  theme_light() +
+  #theme(legend.position = "none") +
+  facet_grid(vars(Treatment), 
+             vars(fun_grp))
+
+
+default_prior(bf(
+  log(voldens) ~ s(ExpDay, by = interaction(fun_grp, Treatment), k = 5) +
+    (ExpDay + fun_grp + ExpDay:fun_grp | mesocosm),
+  sigma ~ fun_grp
+),
+family = gaussian(),
+data = edat_fgroup)
+
+######### compositional changes ################################################
+
+edat_comp = edat_fgroup %>% 
+  
+  select(ExpDay, Treatment, mesocosm, fun_grp, voldens) %>% 
+  pivot_wider(names_from = "fun_grp",
+              values_from = "voldens") %>% 
+  mutate(tot_biov = rowSums(across(c(I,II,III,IV,V,VI,VII)), na.rm = T)) %>% 
+  mutate(across(c(I, II, III, IV, V, VI, VII),
+                ~ . / tot_biov)) %>% 
+  mutate(V = replace_na(V, 1e-06)) %>% 
+  mutate(tot_biov = rowSums(across(c(I,II,III,IV,V,VI,VII)), na.rm = T)) %>% 
+  mutate(across(c(I, II, III, IV, V, VI, VII),
+                ~ . / tot_biov))
+
+edat_comp$Y = with(edat_comp, cbind(I,II,III,IV,V,VI,VII))
+
+mcomp = brm(
+  bf(
+    Y ~ s(ExpDay, by = Treatment, k = 5) +
+       (ExpDay | mesocosm)
+  ),
+  family = dirichlet(),
+  chains = 4,
+  iter = 12000,
+  warmup = 3000,
+  cores = 4,
+  control = list(adapt_delta = 0.99),
+  seed = 543,
+  backend = "cmdstanr", 
+  data = edat_comp
+) 
+pp_check(mgroup, ndraws = 100)
+summary(mgroup)
+
+
+mcomp = brm(
+  bf(
+    Y ~ s(ExpDay, by = Treatment, k = 5) +
+      (ExpDay | mesocosm)
+  ),
+  family = logistic_normal(),
+  prior = prior("lkj(3)", "lncor"),
+  chains = 4,
+  iter = 12000,
+  warmup = 3000,
+  cores = 4,
+  control = list(adapt_delta = 0.99),
+  seed = 543,
+  backend = "cmdstanr", 
+  data = edat_comp
+) 
+pp_check(mgroup, ndraws = 100)
+summary(mgroup)
